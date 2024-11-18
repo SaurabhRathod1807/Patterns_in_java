9fde14ab6004f0da85a96c080b67ce1930d7e598
Saurabh Rathod <rathodsaurabh956@gmail.com>
2024-11-18 13:53:55 +0530

    update

--- Pattern_In_Java/src/patterns/TrianglestarPattern.java
+++ Pattern_In_Java/src/patterns/TrianglestarPattern.java
@@ -22,5 +22,26 @@ public class TrianglestarPattern {
             System.out.println();
         }
         sc.close();
+//        int n = sc.nextInt();
+//        
+//        for(int i=1; i<=n; i++){
+//           for(int j=1; j<=n-1; j++){
+//              System.out.print(" ");
+//           }
+//           for(int j=1; j<=i; j++){
+//              System.out.print("*" + " ");
+//           }
+//           System.out.println();
+//        }
+//        for(int i=n; i>=0; i--){
+//           for(int j=1; j<=n-1; j++){
+//              System.out.print(" ");
+//           }
+//           for(int j=1; j<=i; j++){
+//              System.out.print("*" + " ");
+//           }
+//           System.out.println();
+//        }
+
     }
 }
